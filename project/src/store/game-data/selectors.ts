htmlacademy/guess-melody-demo

8.8. Объединит слайсы в один редьюсер. WIP
Объединим слайсы в общий редьюсер. Для этого создадим файл
`root-reducer.ts`. Затем подключим его при инициализации хранилища.

Сделаем ещё один небольшой рефакторинг. Вместо прямого обращения к
полям хранилища воспользуемся селекторами.

Селектор — обычная функция, которая возвращает значение нужного
поля хранилища.

WIP.  Проект по-прежнему не работает. Мы подготовили всё необходимое,
но не обновили компоненты.

project/src/store/game-data/selectors.ts
@@ -1,0 +1,6 @@
+import {NameSpace} from '../../const';
+import {State} from '../../types/state';
+import {Questions} from '../../types/question';
+
+export const getQuestions = (state: State): Questions => state[NameSpace.data].questions;
+export const getLoadedDataStatus = (state: State): boolean => state[NameSpace.data].isDataLoaded;

project/src/store/game-process/selectors.ts
@@ -1,0 +1,5 @@
+import {NameSpace} from '../../const';
+import {State} from '../../types/state';
+
+export const getStep = (state: State): number => state[NameSpace.game].step;
+export const getMistakeCount = (state: State): number => state[NameSpace.game].mistakes;

project/src/store/root-reducer.ts
@@ -1,0 +1,11 @@
+import {combineReducers} from '@reduxjs/toolkit';
+import {NameSpace} from '../const';
+import {gameData} from './game-data/game-data';
+import {gameProcess} from './game-process/game-process';
+import {userProcess} from './user-process/user-process';
+
+export const rootReducer = combineReducers({
+  [NameSpace.data]: gameData.reducer,
+  [NameSpace.game]: gameProcess.reducer,
+  [NameSpace.user]: userProcess.reducer,
+});

htmlacademy-react/guess-melody-12

project/src/store/game-data/selectors.ts
@@ -1,0 +1,6 @@
+import {NameSpace} from '../../const';
+import {State} from '../../types/state';
+import {Questions} from '../../types/question';
+
+export const getQuestions = (state: State): Questions => state[NameSpace.Data].questions;
+export const getQuestionsDataLoadingStatus = (state: State): boolean => state[NameSpace.Data].isQuestionsDataLoading;

project/src/store/game-process/selectors.ts
@@ -1,0 +1,5 @@
+import {NameSpace} from '../../const';
+import {State} from '../../types/state';
+
+export const getStep = (state: State): number => state[NameSpace.Game].step;
+export const getMistakeCount = (state: State): number => state[NameSpace.Game].mistakes;

project/src/store/root-reducer.ts
@@ -1,0 +1,11 @@
+import {combineReducers} from '@reduxjs/toolkit';
+import {NameSpace} from '../const';
+import {gameData} from './game-data/game-data';
+import {gameProcess} from './game-process/game-process';
+import {userProcess} from './user-process/user-process';
+
+export const rootReducer = combineReducers({
+  [NameSpace.Data]: gameData.reducer,
+  [NameSpace.Game]: gameProcess.reducer,
+  [NameSpace.User]: userProcess.reducer,
+});